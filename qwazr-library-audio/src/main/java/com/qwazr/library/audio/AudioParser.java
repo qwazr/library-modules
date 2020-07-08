80907f6f62e9bd5320beef88969383842980d521
Emmanuel Keller <ekeller@open-search-server.com>
2020-07-08 21:26:52 +0100

    AudioParser implements ParserFactory

--- qwazr-library-audio/src/main/java/com/qwazr/library/audio/AudioParser.java
+++ qwazr-library-audio/src/main/java/com/qwazr/library/audio/AudioParser.java
@@ -15,12 +15,27 @@
  */
 package com.qwazr.library.audio;
 
-import com.qwazr.extractor.ParserAbstract;
+import com.qwazr.extractor.ParserFactory;
 import com.qwazr.extractor.ParserField;
-import com.qwazr.extractor.ParserResult.FieldsBuilder;
-import com.qwazr.extractor.ParserResult.Builder;
+import com.qwazr.extractor.ParserInterface;
+import com.qwazr.extractor.ParserResult;
+import com.qwazr.extractor.ParserUtils;
 import com.qwazr.utils.AutoCloseWrapper;
 import com.qwazr.utils.LoggerUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotSupportedException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import org.apache.commons.lang3.StringUtils;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
@@ -30,79 +45,75 @@ import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagField;
 import org.jaudiotagger.tag.TagTextField;
 
-import javax.ws.rs.NotSupportedException;
-import javax.ws.rs.core.MultivaluedMap;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
-
 final public class AudioParser implements ParserFactory, ParserInterface {
 
     private final static Logger LOGGER = LoggerUtils.getLogger(AudioParser.class);
 
-    private static final HashMap<String, String> MIMEMAP;
-    private static final HashMap<String, String> EXTENSIONMAP;
+    private static final String NAME = "audio";
 
-    private static final Collection<String> DEFAULT_EXTENSIONS;
-    private static final Collection<String> DEFAULT_MIMETYPES;
+    private static final Map<MediaType, String> MIMEMAP;
+    private static final Map<String, MediaType> EXTENSIONMAP;
 
     static {
         MIMEMAP = new HashMap<>();
-        MIMEMAP.put("audio/ogg", "ogg");
-        MIMEMAP.put("audio/mpeg", "mpg");
-        MIMEMAP.put("audio/mpeg3", "mp3");
-        MIMEMAP.put("audio/flac", "flac");
-        MIMEMAP.put("audio/mp4", "mp4");
-        MIMEMAP.put("audio/vnd.rn-realaudio", "ra");
-        MIMEMAP.put("audio/x-pn-realaudio", "ra");
-        MIMEMAP.put("audio/x-realaudio", "ra");
-        MIMEMAP.put("audio/wav", "wav");
-        MIMEMAP.put("audio/x-wav", "wav");
-        MIMEMAP.put("audio/x-ms-wma", "wma");
-
-        DEFAULT_MIMETYPES = MIMEMAP.keySet().toArray(new String[0]);
+        MIMEMAP.put(MediaType.valueOf("audio/ogg"), "ogg");
+        MIMEMAP.put(MediaType.valueOf("audio/mpeg"), "mpg");
+        MIMEMAP.put(MediaType.valueOf("audio/mpeg3"), "mp3");
+        MIMEMAP.put(MediaType.valueOf("audio/flac"), "flac");
+        MIMEMAP.put(MediaType.valueOf("audio/mp4"), "mp4");
+        MIMEMAP.put(MediaType.valueOf("audio/vnd.rn-realaudio"), "ra");
+        MIMEMAP.put(MediaType.valueOf("audio/x-pn-realaudio"), "ra");
+        MIMEMAP.put(MediaType.valueOf("audio/x-realaudio"), "ra");
+        MIMEMAP.put(MediaType.valueOf("audio/wav"), "wav");
+        MIMEMAP.put(MediaType.valueOf("audio/x-wav"), "wav");
+        MIMEMAP.put(MediaType.valueOf("audio/x-ms-wma"), "wma");
 
         EXTENSIONMAP = new HashMap<>();
         MIMEMAP.forEach((k, v) -> EXTENSIONMAP.putIfAbsent(v, k));
-        EXTENSIONMAP.put("m4a", "audio/mp4");
+        EXTENSIONMAP.put("m4a", MediaType.valueOf("audio/mp4"));
     }
 
     private final static Map<FieldKey, ParserField> FIELDMAP;
-    private final static ParserField[] FIELDS;
 
     private final static ParserField FORMAT;
+
+    private final static List<String> SUPPORTED_EXTENSIONS;
 
     static {
         // Build the list of extension for the FORMAT parameter
         StringBuilder sb = new StringBuilder("Supported format: ");
         boolean first = true;
-        DEFAULT_EXTENSIONS = new String[SupportedFileFormat.values().length];
-        int i = 0;
-        for (SupportedFileFormat sff : SupportedFileFormat.values()) {
+        final List<String> extensionList = new ArrayList<>();
+        for (final SupportedFileFormat sff : SupportedFileFormat.values()) {
             if (!first)
                 sb.append(", ");
             else
                 first = false;
             sb.append(sff.getFilesuffix());
-            DEFAULT_EXTENSIONS[i++] = sff.getFilesuffix();
+            extensionList.add(sff.getFilesuffix());
         }
+
+        SUPPORTED_EXTENSIONS = List.copyOf(extensionList);
+
         FORMAT = ParserField.newString("format", sb.toString());
 
         // Build the list of fields returned by the library
         FIELDMAP = new HashMap<>();
         for (FieldKey fieldKey : FieldKey.values())
             FIELDMAP.put(fieldKey, ParserField.newString(fieldKey.name().toLowerCase(), null));
-        FIELDS = FIELDMAP.values().toArray(new ParserField[0]);
-        Arrays.sort(FIELDS, (o1, o2) -> StringUtils.compare(o1.name, o2.name));
     }
 
-    private final static ParserField[] PARAMETERS = { FORMAT };
+    private final static List<ParserField> PARAMETERS = List.of(FORMAT);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public ParserInterface createParser() {
+        return this;
+    }
 
     @Override
     public Collection<ParserField> getParameters() {
@@ -111,31 +122,32 @@ final public class AudioParser implements ParserFactory, ParserInterface {
 
     @Override
     public Collection<ParserField> getFields() {
-        return FIELDS;
+        return FIELDMAP.values();
     }
 
     @Override
     public Collection<String> getSupportedFileExtensions() {
-        return DEFAULT_EXTENSIONS;
+        return SUPPORTED_EXTENSIONS;
     }
 
     @Override
-    public Collection<MediaType> getSupportedMimeTypes {
-        return DEFAULT_MIMETYPES;
+    public Collection<MediaType> getSupportedMimeTypes() {
+        return MIMEMAP.keySet();
     }
 
-    @Override
-    public void parseContent(final MultivaluedMap<String, String> parameters, final Path filePath, String extension,
-            final String mimeType, final ParserResult.Builder resultBuilder) {
+    private ParserResult extract(final Path filePath,
+                                 final MediaType mediaType) throws IOException {
+        final ParserResult.Builder resultBuilder = ParserResult.of(NAME);
         try {
             final AudioFile f = AudioFileIO.read(filePath.toFile());
             final ParserResult.FieldsBuilder metas = resultBuilder.metas();
-            metas.set(MIME_TYPE, findMimeType(extension, mimeType, EXTENSIONMAP::get));
+            if (mediaType != null)
+                metas.set(MIME_TYPE, mediaType.toString());
             final Tag tag = f.getTag();
             if (tag == null)
-                return;
+                return resultBuilder.build();
             if (tag.getFieldCount() == 0)
-                return;
+                return resultBuilder.build();
             for (Map.Entry<FieldKey, ParserField> entry : FIELDMAP.entrySet()) {
                 final List<TagField> tagFields = tag.getFields(entry.getKey());
                 if (tagFields == null)
@@ -146,26 +158,34 @@ final public class AudioParser implements ParserFactory, ParserInterface {
                     metas.add(entry.getValue(), ((TagTextField) tagField).getContent());
                 }
             }
+        } catch (IOException e) {
+            throw e;
         } catch (Exception e) {
-            throw convertException(() -> "Error with " + filePath.toAbsolutePath(), e);
+            throw new InternalServerErrorException(e);
         }
+        return resultBuilder.build();
+    }
+
+
+    @Override
+    public ParserResult extract(final MultivaluedMap<String, String> parameters,
+                                final Path filePath) throws IOException {
+        return extract(filePath, EXTENSIONMAP.get(ParserUtils.getExtension(filePath)));
     }
 
     @Override
-    public void parseContent(final MultivaluedMap<String, String> parameters, final InputStream inputStream,
-            final String extension, final String mimeType, final ParserResult.Builder resultBuilder) {
-        String format = getParameterValue(parameters, FORMAT, 0);
+    public ParserResult extract(final MultivaluedMap<String, String> parameters,
+                                final InputStream inputStream,
+                                final MediaType mimeType) throws IOException {
+        String format = ParserUtils.getParameterValue(parameters, FORMAT, 0);
         if (StringUtils.isEmpty(format))
-            format = extension;
-        if (StringUtils.isEmpty(format) && mimeType != null)
-            format = MIMEMAP.get(mimeType.intern());
+            format = MIMEMAP.get(mimeType);
         if (StringUtils.isEmpty(format))
             throw new NotSupportedException("The format is not found");
         try (final AutoCloseWrapper<Path> a = AutoCloseWrapper.of(
-                ParserAbstract.createTempFile(inputStream, '.' + format), LOGGER, Files::deleteIfExists)) {
-            parseContent(parameters, a.get(), extension, mimeType, resultBuilder);
-        } catch (IOException e) {
-            throw convertIOException(e::getMessage, e);
+                ParserUtils.createTempFile(inputStream, '.' + format), LOGGER,
+                Files::deleteIfExists)) {
+            return extract(a.get(), mimeType);
         }
     }
 
--- qwazr-library-audio/src/test/java/com/qwazr/library/audio/AudioParserTest.java
+++ qwazr-library-audio/src/test/java/com/qwazr/library/audio/AudioParserTest.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2015-2017 Emmanuel Keller / QWAZR
+/*
+ * Copyright 2015-2020 Emmanuel Keller / QWAZR
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,52 +12,59 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
+ */
 package com.qwazr.library.audio;
 
 import com.qwazr.extractor.ExtractorManager;
 import com.qwazr.extractor.ParserTest;
+import javax.ws.rs.core.MediaType;
 import org.junit.Test;
 
 public class AudioParserTest extends ParserTest {
 
-	static final String DEFAULT_TEST_STRING = "osstextextractor";
+    static final String DEFAULT_TEST_STRING = "osstextextractor";
 
-	static final String AUDIO_TEST_STRING = "opensearchserver";
+    static final String AUDIO_TEST_STRING = "opensearchserver";
 
-	public AudioParserTest() {
-		super(new ExtractorManager());
-		manager.registerServices();
-	}
+    public AudioParserTest() {
+        super(new ExtractorManager());
+        manager.registerServices();
+    }
 
-	@Test
-	public void testAudioFlac() throws Exception {
-		doTest(AudioParser.class, "file.flac", "audio/flac", null, AUDIO_TEST_STRING, "format", "flac");
-	}
+    @Test
+    public void testAudioFlac() throws Exception {
+        doTest(AudioParser.class, "file.flac",
+                MediaType.valueOf("audio/flac"), null, AUDIO_TEST_STRING, "format", "flac");
+    }
 
-	@Test
-	public void testAudioM4a() throws Exception {
-		doTest(AudioParser.class, "file.m4a", "audio/mp4", null, DEFAULT_TEST_STRING, "format", "m4a");
-	}
+    @Test
+    public void testAudioM4a() throws Exception {
+        doTest(AudioParser.class, "file.m4a",
+                MediaType.valueOf("audio/mp4"), null, DEFAULT_TEST_STRING, "format", "m4a");
+    }
 
-	@Test
-	public void testAudioMp3() throws Exception {
-		doTest(AudioParser.class, "file.mp3", "audio/mpeg3", null, DEFAULT_TEST_STRING, "format", "mp3");
-	}
+    @Test
+    public void testAudioMp3() throws Exception {
+        doTest(AudioParser.class, "file.mp3",
+                MediaType.valueOf("audio/mpeg3"), null, DEFAULT_TEST_STRING, "format", "mp3");
+    }
 
-	@Test
-	public void testAudioOgg() throws Exception {
-		doTest(AudioParser.class, "file.ogg", "audio/ogg", null, AUDIO_TEST_STRING, "format", "ogg");
-	}
+    @Test
+    public void testAudioOgg() throws Exception {
+        doTest(AudioParser.class, "file.ogg",
+                MediaType.valueOf("audio/ogg"), null, AUDIO_TEST_STRING, "format", "ogg");
+    }
 
-	@Test
-	public void testAudioWav() throws Exception {
-		doTest(AudioParser.class, "file.wav", "audio/wav", null, null, "format", "wav");
-	}
+    @Test
+    public void testAudioWav() throws Exception {
+        doTest(AudioParser.class, "file.wav",
+                MediaType.valueOf("audio/wav"), null, null, "format", "wav");
+    }
 
-	@Test
-	public void testAudioWma() throws Exception {
-		doTest(AudioParser.class, "file.wma", "audio/x-ms-wma", null, AUDIO_TEST_STRING, "format", "wma");
-	}
+    @Test
+    public void testAudioWma() throws Exception {
+        doTest(AudioParser.class, "file.wma",
+                MediaType.valueOf("audio/x-ms-wma"), null, AUDIO_TEST_STRING, "format", "wma");
+    }
 
 }
